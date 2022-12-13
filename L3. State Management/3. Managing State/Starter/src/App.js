ad08f793cf650e1418eba4d454ad1ee889127af1
Abdallah Aboudeif <71098171+aboudeif@users.noreply.github.com>
2022-12-13 10:53:53 +0200

    add likes and like button

--- L3. State Management/3. Managing State/Starter/src/App.js
+++ L3. State Management/3. Managing State/Starter/src/App.js
@@ -1,12 +1,30 @@
+import { useState } from "react";
 import corgi from "./corgi.jpg";
 import "./App.css";
 
 const App = () => {
+  const [likes, setLikes] = useState(0);
+  const [liked, setLiked] = useState(false);
+  const state = [<>&#129293;</>, <>&#10084;&#65039;</>];
   return (
     <div className="container">
       <h2>Like this photo!</h2>
       <img src={corgi} alt="Corgi"></img>
-      <p>Amount of likes: </p>
+      <p>likes: {likes}</p>
+      <button onClick={() => setLikes((prev) => prev + 1)}>Increment people likes</button>
+      <p>Do you like this photo? </p>
+      <div
+        style={{
+          fontSize: "18pt",
+          cursor: "pointer",
+        }}
+        onClick={() => {
+          setLiked(!liked);
+          setLikes((prev) => liked ? prev - 1 : prev + 1);
+        }}
+      >
+        {state[+liked]}
+      </div>
     </div>
   );
 };
