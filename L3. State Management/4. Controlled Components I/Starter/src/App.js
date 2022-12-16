bb7ee8ee6a743c19c437e1480617888674e43e93
Abdallah Aboudeif <71098171+aboudeif@users.noreply.github.com>
2022-12-16 10:38:21 +0200

    finish 4

--- L3. State Management/4. Controlled Components I/Starter/src/App.js
+++ L3. State Management/4. Controlled Components I/Starter/src/App.js
@@ -1,7 +1,10 @@
 import logo from "./logo.svg";
 import "./App.css";
+import { useState } from 'react'
 
 const App = () => {
+  const [text, setText] = useState('')
+  const handelChange = event => setText(event.target.value)
   return (
     <div className="App">
       <header className="App-header">
@@ -9,8 +12,9 @@ const App = () => {
         <h1 className="App-title">ReactND - Coding Practice</h1>
       </header>
       <div className="container">
-        <input type="text" placeholder="Say Something" />
+        <input type="text" placeholder="Say Something" onChange={handelChange} />
         <p className="echo">Echo:</p>
+        <p>{text}</p>
         <p>This should mirror the text you typed into the input field.</p>
       </div>
     </div>
